ff157fa3444c56131d6df566633badaa8ff4e555
mahdi <ghasemim875@gmail.com>
2022-03-25 13:37:17 +0430

    add some comments

--- stack/Stack.php
+++ stack/Stack.php
@@ -1,24 +1,32 @@
 <?php
-
+/**
+ * LIFO rule
+ * array/linked list wrapper
+ * has not lookup method ....
+ */
 class Stack
 {
     private $items = [];
 
+    // O(1)
     public function push($item)
     {
         array_push($this->items, $item);
     }
 
+    // O(1)
     public function pop()
     {
         return array_pop($this->items);
     }
 
+    // O(1)
     public function peek()
     {
         return $this->items[array_key_last($this->items)];
     }
 
+    // O(1)
     public function empty()
     {
         return count($this->items) === 0;

87d0af18d8cb765ab5ab718fc6b38ea1ad2d7080
mahdi <ghasemim875@gmail.com>
2022-03-25 19:02:34 +0430

    solve is balance string by for loop

--- stack/functions.php
+++ stack/functions.php
@@ -13,3 +13,32 @@ function reverseString($str) {
 
     return $newStr;
 }
+
+function checkBalance($str) {
+    $pairs = [ '[' => ']', '(' => ')', '<' => '>', '{' => '}' ];
+
+    $counts = array_fill(0, count($pairs), 0);
+    $opens = array_keys($pairs);
+    $closes = array_values($pairs);
+
+    foreach(str_split($str) as $char) {
+        $index = array_search($char, $opens);
+        if($index !== false) {
+            $counts[$index]++;
+        }
+
+        $index = array_search($char, $closes);
+        if($index !== false) {
+            $counts[$index]--;
+        }
+
+        if($index !== false && $counts[$index] < 0) {
+            return false;
+            break;
+        }
+    }
+
+    $nonZiroCounts = array_filter($counts, fn($count) => $count !== 0);
+
+    return empty($nonZiroCounts);
+}
--- stack/touch.php
+++ stack/touch.php
@@ -3,4 +3,8 @@ require("./stack/Stack.php");
 require("./stack/functions.php");
 
 // Exercise ONE: reverse a string;
-dd(reverseString("mahdi"));
+// dd(reverseString("mahdi"));
+
+// Exercise TWO: balance of string
+$str = "[sdfsd;fnsd<sdfpoj>{}<>]";
+dd(checkBalance($str));

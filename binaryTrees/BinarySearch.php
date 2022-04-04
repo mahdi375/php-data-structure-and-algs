ae37dd04ef1d777d1a41b970dc94d232fdc6b2bb
mahdi <ghasemim875@gmail.com>
2022-04-04 19:42:27 +0430

    min of tree

--- binaryTrees/BinarySearch.php
+++ binaryTrees/BinarySearch.php
@@ -5,6 +5,7 @@ require "./binaryTrees/BinarySearchNode.php";
 class BinarySearch
 {
     private ?BinarySearchNode $root = null;
+    private ?int $min = null;
 
     public function insert(int $value)
     {
@@ -78,6 +79,7 @@ class BinarySearch
 
     private function traverseInOrderRecursion(?BinarySearchNode $root)
     {
+        // TODO: what if want to return array of nodes in order
         if(is_null($root)) {
             return;
         }
@@ -119,5 +121,29 @@ class BinarySearch
         }
 
         return 1 + max([$this->heightRecursion($root->leftNode, $this->heightRecursion($root->rightNode))]);
+    }
+
+    public function min()
+    {
+        $this->minRecursion($this->root);
+        return $this->min;
+    }
+
+    private function minRecursion(?BinarySearchNode $root)
+    {
+        if(is_null($root)) {
+            return;
+        }
+
+        if(is_null($this->min)) {
+            $this->min = $root->value;
+        }
+
+        if($root->value < $this->min) {
+            $this->min = $root->value;
+        }
+
+        $this->minRecursion($root->leftNode);
+        $this->minRecursion($root->rightNode);
     }
 }
--- binaryTrees/touch.php
+++ binaryTrees/touch.php
@@ -10,10 +10,11 @@ $tree->insert(3);
 $tree->insert(6);
 $tree->insert(10);
 $tree->insert(8);  
-$tree->insert(11);  
+$tree->insert(11);
+$tree->insert(1);
 
 // $tree->traversePreOrder(); // 7, 4, 3, 6, 10, 8, 11
 // $tree->traverseInOrder(); // 3,4,6,7,8,10,11
 // $tree->traversePostOrder(); // 3,6,4,8,11,10,7
 
-dd($tree->height()); // 2
+dd($tree->min()); // 3
